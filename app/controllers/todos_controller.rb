d9db64bc72d04e6a29432f501f0a6d407805ee55
ryo27-jp <calvee.20@gmail.com>
2020-08-07 15:08:52 +0900

    Todo登録機能追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,27 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
   end
 
   def show
   end
 
   def new
+    @todo = Todo.new
+  end
+
+  def create
+    todo = Todo.new(todo_params)
+    todo.save!
+    redirect_to todos_url, notice: "Todo「#{todo.name}」を登録しました。"
   end
 
   def edit
   end
+
+  private
+    
+    def todo_params
+      params.require(:todo).permit(:name, :description)
+    end
 end
