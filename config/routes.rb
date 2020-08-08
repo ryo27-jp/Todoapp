7888952f16fe225d82d30022a784f659e74b8fdb
ryo27-jp <calvee.20@gmail.com>
2020-08-08 19:22:42 +0900

    Userモデル・コントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/edit'
+  get 'users/show'
+  get 'users/index'
   root to: 'todos#index'
   resources :todos
   resources :categories
+  resources :users
 end
