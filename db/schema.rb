511e3648ac85af47ba8047f9499f3ede8e446e3b
ryo27-jp <calvee.20@gmail.com>
2020-08-08 15:20:11 +0900

    Categoryモデル・コントローラー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_112149) do
+ActiveRecord::Schema.define(version: 2020_08_08_061400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", limit: 10
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "name", limit: 30, null: false
