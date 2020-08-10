1d4b7288be359d07b49ffb5ca6eceba710ae065c
ryo27-jp <calvee.20@gmail.com>
2020-08-10 22:28:34 +0900

    userモデルにリレーション関係を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :todos
+
+  has_many :viwers, class_name: "User",
+                    foreign_key: "editor_id"
+  belongs_to :edotor, class_name "User", optional: true
 end
