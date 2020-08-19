0d71add27ec7dbfc15aa5ae78cfade0bcd3486c9
ryo27-jp <calvee.20@gmail.com>
2020-08-19 12:17:15 +0900

    エラーメッセージ追加と国際化

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice: 'ログインしました。'
     else
+      flash.now[:alert] = 'メールアドレスもしくはパスワードが違います。'
       render :new
     end
   end
