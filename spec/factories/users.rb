1d9b9022de3b3de057ddb8dcc0f68edd4672d461
ryo27-jp <calvee.20@gmail.com>
2020-09-20 18:30:17 +0900

    bundle

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+      name      {"Aaron"}
+      sequence(:email) { |n| "test#{n}@example.com"}
+      password {"dottle-nouveau-pavilion-tights-furze"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do
-    @user = User.new(
-      name: "Aaron",
-      email:      "test@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-  end
-  it "is valid with a first name, and email, and password" do
-    user = @user
-    expect(user).to be_valid
+  it "有効なファクトリをもつ" do
+    expect(FactoryBot.build(:user)).to be_valid
   end
 
   it "名前がなければ無効" do
-    @user.name = nil
-    user = @user
+    user = FactoryBot.build(:user, name: nil)
     user.valid?
     expect(user.errors.full_messages).to include("名前を入力してください")
   end
   it "emailアドレスがなければ無効" do
-    @user.email = nil
-    user = @user
+    user = FactoryBot.build(:user, email: nil)
     expect(user).not_to be_valid
   end
   it "重複したemailアドレスは無効" do
-    user = User.create(
-      name: "taro",
-      email: "test@example.com",
-      password: "password"
-    )
-    user2 = @user
-    user2.valid?
+    user = FactoryBot.create(:user)
+    user2 = FactoryBot.build(:user)
     expect(user).to be_valid
     expect(user2).not_to be_valid
   end
